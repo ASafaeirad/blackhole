98a766e863257c67f03e9a71c58047233a15343f
Alireza Safaierad <frontendmonster@gmail.com>
2023-10-31 14:46:25 +0400

    fix generator name

--- libs/tools/nx-plugin/src/generators/solid-library/generator.ts
+++ libs/tools/nx-plugin/src/generators/solid-library/generator.ts
@@ -19,7 +19,7 @@ export async function solidLibraryGenerator(
   const options = await normalizeOptions(tree, schema, {
     callingGenerator: '@blackhole/nx:solid-library',
   });
-  const initTask = await initGenerator(tree);
+  const initTask = initGenerator(tree);
   addProject(tree, options);
   createFiles(tree, options);
   addLinting(tree, options);
