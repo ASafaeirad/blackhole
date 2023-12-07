4240f105dee5612277e7f08d39c01c2a363cb839
Alireza Safaierad <frontendmonster@gmail.com>
2023-12-07 21:27:18 +0100

    [BH-56]: Add Storybook (#12)
    * add storybook
    
    * remove comments

---
+++ libs/shared/design/.storybook/preview.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-unresolved
+import 'uno.css';
+
+import type { Preview } from '@storybook/vue3';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
