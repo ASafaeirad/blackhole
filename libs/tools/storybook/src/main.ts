bfdb03711d0a86ca65ec53041eacf0162085562d
ASafaierad <frontendmonster@gmail.com>
2024-02-20 21:17:43 +0100

    format storybook

--- libs/tools/storybook/src/main.ts
+++ libs/tools/storybook/src/main.ts
@@ -1,5 +1,5 @@
-import path from 'node:path';
 import fs from 'node:fs';
+import path from 'node:path';
 
 import { createProjectGraphAsync } from '@nx/devkit';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
