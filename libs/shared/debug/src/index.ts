ad8d5abe8b0a0c20c8e7ba2cae36a9da51055582
ASafaierad <frontendmonster@gmail.com>
2024-02-21 00:42:11 +0100

    split config

--- libs/shared/debug/src/index.ts
+++ libs/shared/debug/src/index.ts
@@ -1,12 +1,11 @@
-import { config } from '@blackhole/config';
-
 import type { DebugScopes, LogLevel } from './Debug';
 import { Debug } from './Debug';
+import { config } from './Debug.config';
 
 export * from './Debug';
 export const debug = new Debug(
   console,
-  config.get('debug.level') as LogLevel | undefined,
-  config.get('debug.scopes') as DebugScopes[],
+  config.get('level') as LogLevel | undefined,
+  config.get('scopes') as DebugScopes[],
   'background-color: #4c6ef5; padding: 4px 8px; color: white;',
 );
