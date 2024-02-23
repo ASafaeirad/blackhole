d5e4a11d811b69a8fbf1ecdf0b0210d965864687
ASafaierad <frontendmonster@gmail.com>
2024-02-23 17:02:55 +0100

    remove store provider

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,16 +1,13 @@
 import { keyMaps } from '@blackhole/actions';
 import { KeyBindingProvider } from '@blackhole/keybinding-manager';
-import { StoreProvider } from '@blackhole/store';
 
 import { Routes } from './Routes';
 
 const App = () => {
   return (
-    <StoreProvider>
-      <KeyBindingProvider keyMaps={keyMaps}>
-        <Routes />
-      </KeyBindingProvider>
-    </StoreProvider>
+    <KeyBindingProvider keyMaps={keyMaps}>
+      <Routes />
+    </KeyBindingProvider>
   );
 };
 
