a1550fa6939a9364c07d69505d111d35834a31e8
ASafaierad <frontendmonster@gmail.com>
2024-02-23 11:49:27 +0100

    remove unused generators

--- libs/tools/nx-plugin/src/generators/solid-library/schema.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NormalizedProjectSchema, ProjectSchema } from '../shared/schema';
-
-export interface SolidLibrarySchema extends ProjectSchema {}
-
-export interface NormalizedSchema
-  extends Omit<SolidLibrarySchema, 'importPath'>,
-    NormalizedProjectSchema {}
--- libs/tools/nx-plugin/src/generators/vue-library/schema.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NormalizedProjectSchema, ProjectSchema } from '../shared/schema';
-
-export interface VueLibrarySchema extends ProjectSchema {}
-
-export interface NormalizedSchema
-  extends Omit<VueLibrarySchema, 'importPath'>,
-    NormalizedProjectSchema {}
