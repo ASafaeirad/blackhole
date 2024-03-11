b23ad7afb867eb50aba63c8cd1a6ed17ec541a90
Alireza Safaierad <frontendmonster@gmail.com>
2024-03-11 03:31:15 +0000

    fix: fix overflow task name layout

--- libs/task/task-list/src/components/TaskName.tsx
+++ libs/task/task-list/src/components/TaskName.tsx
@@ -15,19 +15,20 @@ export const TaskName = ({ name, focus }: Props) => {
     <div className="fr f1 flex-shrink-1 gap-3 items-center">
       {items.map((item, index) =>
         !isLastIndex(items, index) ? (
-          <Fragment key={index}>
+          <span
+            className={cn('self-start fr gap-3', {
+              'op-70': !focus,
+              'op-90': focus,
+            })}
+            key={index}
+          >
             <span
-              className={cn('text-small py-1 px-3 rounded bg-current-subtle', {
-                'op-70': !focus,
-                'op-90': focus,
-              })}
+              className={cn('text-small py-1 px-3 rounded bg-current-subtle')}
             >
               {item}
             </span>
-            <span className={cn({ 'op-70': !focus, 'op-90': focus })}>
-              &gt;
-            </span>
-          </Fragment>
+            <span>&gt;</span>
+          </span>
         ) : (
           <span key={index}>{item}</span>
         ),
