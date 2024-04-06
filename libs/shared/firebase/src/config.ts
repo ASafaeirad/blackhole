f884b02a3efa8d48b90393972920380a9bb1193d
Alireza Safaierad <frontendmonster@gmail.com>
2024-04-06 19:41:34 +0200

    chore: rename env variable for firebase

--- libs/shared/firebase/src/config.ts
+++ libs/shared/firebase/src/config.ts
@@ -10,10 +10,10 @@ export const config = new Config({
 });
 
 config.parse({
-  apiKey: import.meta.env['VITE_API_KEY'],
-  authDomain: import.meta.env['VITE_AUTH_DOMAIN'],
-  projectId: import.meta.env['VITE_PROJECT_ID'],
-  storageBucket: import.meta.env['VITE_STORAGE_BUCKET'],
-  messagingSenderId: import.meta.env['VITE_MESSAGING_SENDER_ID'],
-  appId: import.meta.env['VITE_APP_ID'],
+  apiKey: import.meta.env['VITE_FIREBASE_API_KEY'],
+  authDomain: import.meta.env['VITE_FIREBASE_AUTH_DOMAIN'],
+  projectId: import.meta.env['VITE_FIREBASE_PROJECT_ID'],
+  storageBucket: import.meta.env['VITE_FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: import.meta.env['VITE_FIREBASE_MESSAGING_SENDER_ID'],
+  appId: import.meta.env['VITE_FIREBASE_APP_ID'],
 });
