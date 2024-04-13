96a5f81b197bfea0ce5ab4e6291fbe0b1b1ad51a
Alireza Safaierad <frontendmonster@gmail.com>
2024-04-14 00:16:20 +0200

    chore: remove supabase

--- libs/shared/supabase/src/index.ts
+++
@@ -1,2 +0,0 @@
-export type * from './lib/Database';
-export { supabaseClient as supabase } from './lib/supabaseClient';
--- libs/shared/supabase/supabase/migrations/20231226152216_init.sql
+++
@@ -1,1 +0,0 @@
-create extension if not exists "moddatetime" with schema "extensions";
